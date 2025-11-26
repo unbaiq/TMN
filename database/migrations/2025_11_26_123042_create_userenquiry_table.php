3ccda4c8a864fff6a6aa3021d18c8175e4652f77
Tushar Sharma <tusharsharma97177@gmail.com>
2025-11-26 18:06:24 +0530

    tushar

---
+++ database/migrations/2025_11_26_123042_create_userenquiry_table.php
@@ -0,0 +1,36 @@
+
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('enquiries', function (Blueprint $table) {
+            $table->id();
+
+            // Basic enquiry details
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('contact_number', 20)->nullable();
+            $table->string('profession')->nullable();
+
+           
+
+            // Status of enquiry
+            $table->enum('status', ['new', 'in_progress', 'closed'])->default('new');
+
+           
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('enquiries');
+    }
+};
